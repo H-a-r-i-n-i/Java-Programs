a54cdc1e6ae20dfaf2038f81ea41d519b19e8bba
H-a-r-i-n-i <118799405+H-a-r-i-n-i@users.noreply.github.com>
2023-12-27 11:31:16 +0530

    Update Quiz1_3.java

--- Day2/Quiz1_3.java
+++ Day2/Quiz1_3.java
@@ -1,14 +1,68 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Day2;
 
-/**
- *
- * @author HP
- */
+package Day2;
+    import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Scanner;
 public class Quiz1_3 {
-    
+
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of words: ");
+        int n = scanner.nextInt();
+        scanner.nextLine();  
+
+        String[] words = new String[n];
+        System.out.println("Enter the words:");
+        for (int i = 0; i < n; i++) {
+            words[i] = scanner.nextLine();
+        }
+
+        List<String> concatenatedWords = findAllConcatenatedWords(words);
+        System.out.println(concatenatedWords);
+    }
+
+    public static List<String> findAllConcatenatedWords(String[] words) {
+        List<String> result = new ArrayList<>();
+        Set<String> wordSet = new HashSet<>();
+        for (String word : words) {
+            wordSet.add(word);
+        }
+
+        for (String word : words) {
+            wordSet.remove(word);
+            if (canFormConcatenatedWord(word, wordSet)) {
+                result.add(word);
+            }
+            wordSet.add(word);
+        }
+
+        return result;
+    }
+
+    private static boolean canFormConcatenatedWord(String word, Set<String> wordSet) {
+        if (wordSet.isEmpty()) {
+            return false;
+        }
+
+        int n = word.length();
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && wordSet.contains(word.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
 }
+
+
