56dee0bea438b9d6c2129bd6a584c2b009bcd81e
H-a-r-i-n-i <118799405+H-a-r-i-n-i@users.noreply.github.com>
2024-02-10 14:26:17 +0530

    Create 2.java

---
+++ set1/2.java
@@ -0,0 +1,18 @@
+public class Pattern2{
+  public static void main(String args[]){
+    Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
+
+    for(int i=1;i<=n;i++)
+      {
+        for(int s=n-1;n>=i;s--)
+           System.out.print(" ");
+        for(int j=1;j<=i;j++)
+          {
+            System.out.print(j);
+          }
+        System.out.println();
+      }
+  }
+}
