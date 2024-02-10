013ef21fa64fdbb03ecc4c31b2c1ebf4d4afac4b
H-a-r-i-n-i <118799405+H-a-r-i-n-i@users.noreply.github.com>
2024-02-10 14:16:17 +0530

    Create 7.java

---
+++ Set2/7.java
@@ -0,0 +1,23 @@
+public class permutation{
+	static void printPermutn(String str, String ans)
+	{
+    if (str.length() == 0) {
+			System.out.print(ans + " ");
+			return;
+		}
+
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			String ros = str.substring(0, i) +
+						str.substring(i + 1);
+			printPermutn(ros, ans + ch);
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		Scanner obj=new Scanner(System.in);
+    String s=obj.next();
+		printPermutn(s, "");
+	}
+}
