d6e92954dfacd8beb703ce299d1b149cf37f2768
H-a-r-i-n-i <118799405+H-a-r-i-n-i@users.noreply.github.com>
2024-02-10 15:01:38 +0530

    Create 10.java

---
+++ set1/10.java
@@ -0,0 +1,27 @@
+
+public class ZPattern {
+
+    public static void main(String args[]) {
+        Scanner obj = new Scanner(System.in);
+        String str;
+        System.out.println("Enter the string");
+        str = obj.next();
+        int n = str.length();
+        int l1 = (n / 3) + 1, l2 = l1 - 2;
+        int  temp2 = l2;
+        for (int i = 0; i < n; i++) {
+            if (i >= l1 && temp2 >= 0) {
+                System.out.println();
+                for (int s = temp2; s >= 1; s--) {
+                    System.out.print("  ");
+                }
+                System.out.print(str.charAt(i)+" ");            
+                temp2--;
+            } else {
+                System.out.print(str.charAt(i)+" ");
+            }
+
+        }
+    }
+}
+
