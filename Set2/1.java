e7fcfb731951eb59071e0bb1cbb7d3385310bf41
H-a-r-i-n-i <118799405+H-a-r-i-n-i@users.noreply.github.com>
2024-02-10 13:56:25 +0530

    Create 1.java

---
+++ Set2/1.java
@@ -0,0 +1,35 @@
+package zoho;
+
+import java.util.Scanner;
+
+public class a4b3Reverse {
+
+    public static void main(String[] args) {
+         Scanner obj=new Scanner(System.in);
+         System.out.println("Enter string");
+   String str=obj.next();
+        int n = str.length();
+        int count = 1, j;
+        String num = "";
+        for (int i = 0; i < n; i++) {
+            if (!(str.charAt(i) >= 'a' && str.charAt(i) <= 'z')) {
+                j = i;
+                while (!(str.charAt(j) >= 'a' && str.charAt(j) <= 'z')) {
+                    num += str.charAt(j);
+                    j++;
+                    if (j == n) {
+                        break;
+                    }
+                }
+                count = Integer.parseInt(num);
+                for (int c = 0; c < count; c++) {
+                    System.out.print(str.charAt(i - 1));
+                }
+
+                i = j;
+
+                num = "";
+            }
+        }
+    }
+}
